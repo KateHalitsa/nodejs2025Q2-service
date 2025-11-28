cf67fade535c3a4bc7c0c4ee4caf157dbeb049ae
Katya <kate.halitsa@gmail.com>
2025-11-28 15:07:38 +0300

    feat: add user implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+/*import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+*/
+import { UserModule } from './modules/user/user.module';
+/*import {AppController} from "./app.controller";*/
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  /*controllers: [AppController],
+  providers: [UserModule],*/
 })
 export class AppModule {}
